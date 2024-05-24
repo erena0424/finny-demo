34f4e526eaa17908f071d78956af643a9eea6f4d
erena0424 <124848190+erena0424@users.noreply.github.com>
2024-05-24 09:44:20 +0900

    first commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'  # Use environment variable
     MAIL_SERVER= 'smtp.googlemail.com' 
-    MAIL_PORT= 587
+    MAIL_PORT=  # Add mail port
     MAIL_USE_TLS = True
     MAIL_USERNAME = '' # Add the email address to send the password reset email
     MAIL_PASSWORD = '' # Add the password to allow this app to send the email, if needed
